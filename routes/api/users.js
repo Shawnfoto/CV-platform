68c201f7659c4cf694978fba42286a47e56cd5e3
Shawn <rpd11140@gmail.com>
2018-12-26 21:24:16 +0800

    Prepared for deploy

--- routes/api/users.js
+++ routes/api/users.js
@@ -72,7 +72,7 @@ router.post("/login", (req, res) => {
         jwt.sign(
           payload,
           keys.secretOrKey,
-          { expiresIn: 30 },
+          { expiresIn: 3600 },
           (err, token) => {
             res.json({
               success: true,
